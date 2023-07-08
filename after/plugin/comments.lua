bcdf2bc33ca665d3dbb16f7788dbdae7504a3fc9
RobertPietraru <rob_piet@yahoo.com>
2023-07-08 15:25:51 +0300

    <leader>/ for comments, including multiline

--- after/plugin/comments.lua
+++ after/plugin/comments.lua
@@ -4,6 +4,7 @@ local comments = require('Comment.api')
 
 -- vim.fn.visualmode()
 vim.keymap.set("n", "<leader>/", function () comments.toggle.linewise() end)
+vim.keymap.set("v", "<leader>/", "<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>")
 
 
 
